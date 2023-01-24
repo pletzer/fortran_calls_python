3226981e2b67d7c886c6b0e16b3f36810dedf148
Alex Pletzer <alexander@gokliya.net>
2023-01-24 17:09:50 +1300

    working on the netcdf write

--- src/self_descr_array_mod.f90
+++ src/self_descr_array_mod.f90
@@ -33,4 +33,35 @@ subroutine sda_destroy(obj)
     deallocate(obj%dims)
 end subroutine sda_destroy
 
+subroutine sda_define_data(obj, ncid, varid)
+    use netcdf
+    implicit none
+    type(self_descr_array_type) :: obj
+    integer, intent(in) :: ncid
+    integer, intent(out) :: varid
+    integer :: status, nc_type, ndims, i
+    integer, allocatable :: dim_ids(:)
+    character(len=128) :: dim_name, i_str
+
+    ! define the dimensions
+    ndims = size(obj%dims)
+    allocate(dim_ids(ndims))
+    do i = 1, ndims
+        write(i_str, '(A)') i
+        dim_name = obj%name // i_str
+        status = nf90_def_dim(ncid, dim_name, obj%dims(i), dim_ids(i))
+        if(status /= nf90_noerr) call handle_error(status)
+    enddo
+
+    ! define the variable
+    nc_type = nf90_double
+    if (obj%type == 'i') then
+        nc_type = nf90_int
+    endif
+    status = nf90_def_var(ncid, obj%name, nc_type, dim_ids, varid)
+    if(status /= nf90_noerr) call handle_error(status)
+
+
+end subroutine sda_define_data
+
 end module self_descr_array_mod
