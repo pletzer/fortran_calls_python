77005a543474441400f1fcf3190054c276911af9
Alex Pletzer <alexander@gokliya.net>
2023-01-24 15:37:38 +1300

    fixed the test

--- src/self_descr_array_mod.f90
+++ src/self_descr_array_mod.f90
@@ -10,30 +10,27 @@ end type self_descr_array_type
 
 contains
 
-function sda_create(name, type, dims, data_address) result(obj)
+subroutine sda_create(obj, name, type, dims, data_address)
     use iso_c_binding, only: c_loc, c_ptr
     implicit none
+    type(self_descr_array_type) :: obj
     character(len=*), intent(in) :: name
     character(len=*), intent(in) :: type
     integer :: dims(:)
     type(c_ptr), value :: data_address
 
-    type(self_descr_array_type), pointer :: obj
-    allocate(obj)
-    
-    ! obj%name = name
-    ! obj%type = type
-    ! allocate(obj%dims(size(dims)))
-    ! obj%dims = dims
-    ! obj%address = data_address
+    obj%name = name
+    obj%type = type
+    allocate(obj%dims(size(dims)))
+    obj%dims = dims
+    obj%address = data_address
 
-end function sda_create
+end subroutine sda_create
 
 subroutine sda_destroy(obj)
     implicit none
-    type(self_descr_array_type), pointer :: obj
+    type(self_descr_array_type) :: obj
     deallocate(obj%dims)
-    deallocate(obj)
 end subroutine sda_destroy
 
 end module self_descr_array_mod
--- tests/test_self_descr_array.f90
+++ tests/test_self_descr_array.f90
@@ -3,11 +3,11 @@ program test_self_descr_array
     use self_descr_array_mod
     implicit none
     real(8), pointer :: a(:, :)
-    type(self_descr_array_type), pointer :: obj
+    type(self_descr_array_type) :: obj
 
     allocate(a(2, 3))
-    obj = sda_create('a', 'r8', shape(a), c_loc(a))
-    ! call sda_destroy(obj)
+    call sda_create(obj, 'a', 'r8', shape(a), c_loc(a))
+    call sda_destroy(obj)
 
 
 
